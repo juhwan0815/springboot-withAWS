5b85cb5aa64de80c46b380f19cb7d69fb37383b3
juhwan0815 <juwom0831@naver.com>
2021-02-26 18:28:29 +0900

    [#3] 스프링부트에서 JPA로 데이터베이스 다뤄보기

--- src/main/java/juhwan/study/spring/Application.java
+++ src/main/java/juhwan/study/spring/Application.java
@@ -2,7 +2,9 @@ package juhwan.study.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
 
