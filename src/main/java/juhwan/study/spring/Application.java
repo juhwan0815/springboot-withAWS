3aaa22de2eaf8915d32528075c37b6dc951afd75
juhwan0815 <juwom0831@naver.com>
2021-08-31 16:25:17 +0900

    스프링 부트와 AWS로 혼자 구현하는 웹 서비스 복습 : 인텔리제이로 스프링 부트 시작하기

--- src/main/java/juhwan/study/spring/Application.java
+++ src/main/java/juhwan/study/spring/Application.java
@@ -2,13 +2,12 @@ package juhwan.study.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
