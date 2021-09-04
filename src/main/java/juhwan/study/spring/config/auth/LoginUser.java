2aa80ebfe2de8c4fa6153b5c1b320e51e310cdce
juhwan0815 <juwom0831@naver.com>
2021-09-04 22:04:26 +0900

    스프링 부트와 AWS로 혼자 구현하는 웹 서비스 복습 : 스프링 시큐리티와 OAuth2.0으로 로그인 기능 구현하기

---
+++ src/main/java/juhwan/study/spring/config/auth/LoginUser.java
@@ -0,0 +1,13 @@
+package juhwan.study.spring.config.auth;
+
+import org.springframework.data.mapping.callback.ReactiveEntityCallbacks;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginUser {
+}
--- src/main/java/juhwan/study/spring/web/IndexController.java
+++ src/main/java/juhwan/study/spring/web/IndexController.java
@@ -1,22 +1,33 @@
 package juhwan.study.spring.web;
 
+import juhwan.study.spring.config.auth.LoginUser;
+import juhwan.study.spring.config.auth.dto.SessionUser;
 import juhwan.study.spring.service.posts.PostsService;
 import juhwan.study.spring.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
+
+        if(user != null){
+            model.addAttribute("userName",user.getName());
+        }
+
         return "index";
     }
 
